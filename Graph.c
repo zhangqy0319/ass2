4eb92406a5c545799267325bcef53ba63db751aa
yycTom <860343832@qq.com>
2019-04-08 17:20:55 +1000

    4月8号17：30完成

--- Graph.c
+++ Graph.c
@@ -44,7 +44,7 @@ bool adjacent(Graph g, Vertex src, Vertex dest) {
 }
 
 AdjList outIncident(Graph g, Vertex v) {
-	assert g != NULL;
+	assert (g != NULL);
 	AdjList *list_of_outgoing_nodes; // create a list which is uesd to store outIncident nodes
 	int j = 0;
 	while(g->NodeList[j] != NULL) {
@@ -55,23 +55,45 @@ AdjList outIncident(Graph g, Vertex v) {
 		j++;
 	}
 	int i = 0;
-	int j = 0;
+	while(vnode->next[i] != NULL) {
+		list_of_outgoing_nodes[i] = vnode->next[i];
+		i++;
+	}
+	return list_of_outgoing_nodes;
+}
+
+AdjList inIncident(Graph g, Vertex v) {
+	assert (g != NULL);
+	AdjList *list_of_incoming_nodes; // create a list which is uesd to store outIncident nodes
+	int j = 0; 
+	int i = 0;
+	int m = 0;
 	while(g->NodeList[j] != NULL) {
-		if(vnode->next->w == g->NodeList[j]->w) {
-			list_of_outgoing_nodes[i] = g->NodeList[j];
+		while(g->NodeList[j]->next[i] != NULL) {
+			if(g->NodeList[j]->next[i]->w == v) {
+				list_of_incoming_nodes[m] = g->NodeList[j];
+				m++;
+			}
 			i++;
 		}
 		j++;
 	}
-	return *list_of_outgoing_nodes;
-}
-
-AdjList inIncident(Graph g, Vertex v) {
-	return NULL;
+	return list_of_incoming_nodes;
 }
 
 void  showGraph(Graph g) {
-
+	assert (g != NULL);
+	printf ("The number of V=%d, The number of E=%d\n", g->nV, g->nE);
+	int i = 0;
+	int j = 0;
+	while(g->NodeList[i] != NULL) {
+		while(g->NodeList[i]->next[j] != NULL) {
+			printf("From Vertex [%d] to Vertex [%d], the weight is %d\n", 
+			g->NodeList[i]->w, g->NodeList[i]->next[j]->w, g->NodeList[i]->weight);
+			j++;
+		}
+		i++;
+	}
 }
 
 void  freeGraph(Graph g) {
