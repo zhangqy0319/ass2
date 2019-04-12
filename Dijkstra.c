8ecc4ffc0272e8bab8517dab62fb3f1ad8e91d1f
LawrenceLin2333 <562775064@qq.com>
2019-04-12 10:41:23 +1000

    Small add

--- Dijkstra.c
+++ Dijkstra.c
@@ -5,24 +5,49 @@
 #include <assert.h>
 #include <stdio.h>
 
-typedef struct PredNode *PredNodep;
+#define INF 2147483647
+
+typedef struct PredNode *PredNodePtr;
+
+static ShortestPaths newShortestPaths(Graph, Vertex);
+static PredNodePtr newPredNode(int);
+
 
 ShortestPaths dijkstra(Graph g, Vertex v) {
 	assert(g != NULL);
 	ShortestPaths throwAway = {0};
-	throwAway.noNodes = 0;
-	throwAway.src = v;
-	throwAway.dist = malloc(numVerticies(g) * sizeof(int));
-	throwAway.pred = malloc(numVerticies(g) * sizeof(PredNodep));
-
+	throwAway = newShortestPaths(g, v);
+	throwAway.dist[v] = 0; // Set distance to src = 0
+	
 	return throwAway;
 }
 
 void showShortestPaths(ShortestPaths paths) {
-
+	return;
 }
 
 
 void  freeShortestPaths(ShortestPaths paths) {
+	return;
+}
 
+static ShortestPaths newShortestPaths(Graph g, Vertex v){
+	ShortestPaths new;
+	int numnode = numVerticies(g);
+	new.noNodes = numnode;
+	new.src = v;
+	new.dist = malloc(numnode * sizeof(int));
+	assert(new.dist != NULL);
+	for (int i = 0; i < numnode; i++) new.dist[i] = INF;
+	new.pred = malloc(numnode * sizeof(PredNodePtr));
+	assert(new.pred != NULL);
+	for (int i = 0; i < numnode; i++) new.pred[i] = NULL;
+	return new;
+}
+
+static PredNodePtr newPredNode(int v){
+	PredNodePtr new = malloc(sizeof(PredNodePtr));
+	new->v = v;
+	new->next = NULL;
+	return new;
 }
